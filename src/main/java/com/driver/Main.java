64add307a73bae25a195be74e890f4f688ea7936
Kishor Kumar <kishorpatel1606@gmail.com>
2025-09-22 19:48:10 +0530

    Polimorphism assignment solved

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,19 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args){
+        Product p = new Product();
+        p.product(2,8);
+        p.product(5,6,7);
+        p.product(4.50,2.50);
+    }
+
+class Product{
+    public int product(int x, int y) {}
+    public int product(int x, int y, int z) {}
+    public double product(double x, double y) {}
+
+
+}
 
 }
